0f862304c13eb5e2df0bbedb766912ec2629a0a8
Hardik585 <Hardikkaushik83126@gmail.com>
2024-09-18 23:10:53 +0530

    escape character " and \' case generate and learn purpose of them

---
+++ week2/day8/NameInQuote.java
@@ -0,0 +1,13 @@
+class NameInQuote{
+  public static void main(String[] args){
+    
+       System.out.println("Name in Double Quote ");
+       System.out.println("\"Hardik\"");
+       System.out.println("Name in Single Quote");
+       System.out.println("\'Hardik\'");
+       System.out.println("We can use single quote inside the double quote ");
+       System.out.println("'Hardik'");
+    
+  }
+
+}
