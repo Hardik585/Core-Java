e86c400192689899e94a3ccb7313d7a67e0a370c
Hardik585 <Hardikkaushik83126@gmail.com>
2024-10-05 12:42:10 +0530

    diff type of print mehtod

---
+++ week2/day10/DifferPrint.java
@@ -0,0 +1,10 @@
+class DifferPrint{
+
+public static void main(String[] args){
+   System.out.print("HK, ");
+   System.out.println("NiT");
+   System.out.print("Ameerpet, ");
+   System.out.println("Hydrabad");
+   System.out.println("Best for FSJD");
+}
+}
